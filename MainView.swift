bf4da08299efd2f77d948606bc8817b87d063566
alysonabril <alysonabril@pursuit.org>
2019-01-25 15:37:14 -0500

    added textfield, and it uploads successfully

--- MainView.swift
+++ MainView.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class MainView: UIView {
 
+    lazy var textField: UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = .white
+        tf.textColor = .black
+        return tf
+    }()
     
-
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        addSubview(textField)
+        setContraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setContraints() {
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        textField.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -45).isActive = true
+        textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 45).isActive = true
+    }
+    
 }
